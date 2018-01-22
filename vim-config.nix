b5d68e9090b37eea2d4c645e85db7f726a42bffb
RdN-MSF <ramses.de.norre@brussels.msf.org>
2018-01-22 11:55:32 +0100

    Try git config in nix config.

---
+++ vim-config.nix
@@ -0,0 +1,61 @@
+with import <nixpkgs> {};
+
+vim_configurable.customize {
+    # Specifies the vim binary name.
+    # E.g. set this to "my-vim" and you need
+    # to type "my-vim" to open this vim
+    # This allows to have multiple vim packages installed
+    # (e.g. with a different set of plugins)
+    name = "vim";
+    vimrcConfig.customRC = ''
+        # Here one can specify what usually goes into `~/.vimrc`
+
+        set nocompatible
+        set modeline
+        set backspace=2
+        set showmode
+        set autoindent
+        filetype on
+        filetype plugin on
+        filetype indent on
+        if has("autocmd")
+          au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$")
+            \| exe "normal! g`\"" | endif
+        endif
+
+        set encoding=utf-8
+        set termencoding=utf-8
+
+        if has('syntax')
+          syntax on
+        endif
+
+        colorscheme desert
+
+        set ruler
+        set number
+
+        if version >= 700
+          set sursorline
+        endif
+
+        set laststatus=2
+        set statusline=%-3.3n\ %f%(\ %r%)%(\ %#WarningMsg#%m%0*%)%=(%l/%L,\ %c)\ %P\ [%{&encoding}:%{&fileformat}]%(\ %w%)\ %y\
+
+        set shortmess+=axr
+
+        set showmatch
+
+        set tabstop=4
+        set shiftwidth=4
+        set softtabstop=4
+        set expandtab
+        set wrapmargin=0
+
+        set nohlsearch
+        set ignorecase
+        set smartcase
+        set incsearch
+    '';
+}
+
