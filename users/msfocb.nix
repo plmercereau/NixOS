8a87016a1ba0dd2a9fe744aee9c8929cc3bf8805
RdN <RdN-MSF@users.noreply.github.com>
2018-06-30 23:10:58 +0000

    Remove public keys for msfocb user.

--- users/msfocb.nix
+++ users/msfocb.nix
@@ -6,9 +6,7 @@
     extraGroups = [ "wheel" "networkmanager" ];
     # python2 -c 'import crypt, getpass,os,base64; print crypt.crypt(getpass.getpass(), "$6$"+base64.b64encode(os.urandom(16))+"$")'
     hashedPassword = "$6$fdEP2xGs$2qw.bg8Mb5ohQvIl3UAbr65Mi9C.m4qXs9R.Vc7TqZVemxt3AfF5oQNNZZwbyYd/MrVd2UMGjW4jQAcYFvgLJ/";
-    openssh.authorizedKeys.keyFiles = [
-      ../keys/msfocb
-    ];
+    openssh.authorizedKeys.keyFiles = [];
   };
 }
 
